2eec4eb0bf315154ee62ec3f39a3b92fa14c5f25
qpwoei0123 <qpwoei0123@naver.com>
2023-12-23 20:31:46 +0900

    chore: 테일윈드 설치

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'; // * BrowserRouter 불러오기
-import './index.css';
 import App from './App';
+import './styles/tailwind.css';
 
 // * App 을 BrowserRouter 로 감싸기
 ReactDOM.render(
