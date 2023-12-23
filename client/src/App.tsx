602792ff980578552c3d95ac906e532460a608c1
qpwoei0123 <qpwoei0123@naver.com>
2023-12-24 05:02:51 +0900

    feat: 메인페이지 구조 구현

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,17 @@ import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import Header from './component/Header';
+import Footer from './component/Footer';
 
 function App() {
   return (
-    <div className="h-[200vh]">
+    <div className="w-screen">
       <Header />
       <Routes>
         <Route path="/" Component={Home} />
         <Route path="/Login" Component={Login} />
       </Routes>
+      <Footer />
     </div>
   );
 }
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,11 +3,29 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div className="text-3xl font-bold underline text-yellow-500">
-      <h1>홈</h1>
-      <p>이곳은 홈이에요. 가장 먼저 보여지는 페이지죠.</p>
-      <Link to="/login">로그인으로</Link>
-    </div>
+    <section>
+      <section className="w-full py-6 sm:py-12 md:py-24 lg:py-32 xl:py-48 ">
+        <div className="container mx-5 md:mx-20">
+          <div className="flex flex-col justify-center space-y-4">
+            <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
+              Connect with Magnet
+            </h1>
+            <p className=" text-gray-500 text-xs md:text-xl">
+              당신의 커리어 여정을 안내해 줄 완벽한 멘토를 찾아보세요.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100">
+        <div className="flex flex-col items-center justify-center space-y-4 ">
+          <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Meet our Top Mentors</h2>
+          <p className="max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+            멘티의 성공을 위해 노력하는 경험 많은 멘토를 만나보세요.
+          </p>
+        </div>
+      </section>
+    </section>
   );
 };
 
