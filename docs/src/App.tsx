fabda2d3762532370aa41a3f732188bcab869904
qpwoei0123 <qpwoei0123@naver.com>
2023-12-28 15:25:14 +0900

    feat: 라우팅 추가

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import LoginEmail from './pages/LoginEmail';
 import Header from './component/Header';
 import Footer from './component/Footer';
 
 function App() {
   return (
-    <div className="w-screen">
+    <div>
       <Header />
       <Routes>
         <Route path="/magnet" Component={Home} />
-        <Route path="/Login" Component={Login} />
+        <Route path="/login" Component={Login} />
+        <Route path="/loginemail" Component={LoginEmail} />
       </Routes>
       <Footer />
     </div>
