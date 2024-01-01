0afbc8b34a2793eca85126a5def720c2dc3c5392
qpwoei0123 <qpwoei0123@naver.com>
2024-01-01 21:27:24 +0900

    feat: 로그인 버튼 타입 분류

--- docs/src/component/auth/LoginButton.tsx
+++ docs/src/component/auth/LoginButton.tsx
@@ -1,11 +1,52 @@
 import React from 'react';
+import kakaoImage from '../../asset/logo/kakao.png';
+import googleImage from '../../asset/logo/google.png';
 import { Link } from 'react-router-dom';
 
-const LoginButton = () => {
+interface LoginButtonProps {
+  type: 'Email' | 'Google' | 'Kakao';
+}
+
+const LoginButton: React.FC<LoginButtonProps> = ({ type }) => {
+  let buttonStyle = '';
+  let buttonText = '';
+  let buttonImage = '';
+  let linkTo = '';
+
+  switch (type) {
+    case 'Email':
+      buttonStyle = 'bg-blue-500 text-white';
+      buttonText = '이메일로 로그인 하기';
+      linkTo = '/loginemail';
+      break;
+    case 'Google':
+      buttonStyle = 'bg-slate-100 text-black';
+      buttonText = 'Google로 계속하기';
+      buttonImage = googleImage;
+      // 나중에 리다이렉션 추가해!
+      linkTo = '';
+      break;
+    case 'Kakao':
+      buttonStyle = 'bg-yellow-400 text-black';
+      buttonText = 'Kakao로 계속하기';
+      buttonImage = kakaoImage;
+      // 나중에 리다이렉션 추가해!
+      linkTo = '';
+      break;
+    default:
+      buttonStyle = 'bg-blue-500 text-white';
+      buttonText = '이메일로 로그인 하기';
+      linkTo = '/loginemail';
+      break;
+  }
+
   return (
-    <div className=" w-80 h-12 flexCenter bg-white border-[1px] rounded-xl cursor-pointer">
-      <p className="font-semibold">아무 계정으로 계속하기</p>
-    </div>
+    <Link to={linkTo}>
+      <div className={`w-80 h-12 flexCenter border-[1px] rounded-xl cursor-pointer ${buttonStyle}`}>
+        {buttonImage && <img src={buttonImage} alt={`${type} 이미지`} className="mr-2 w-6 " />}
+        <p className="font-semibold">{buttonText}</p>
+      </div>
+    </Link>
   );
 };
 
--- docs/src/pages/LoginPage.tsx
+++ docs/src/pages/LoginPage.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import LoginButton from '../component/auth/LoginButton';
-import { Link } from 'react-router-dom';
 
 const LoginPage = () => {
   return (
     <div className="w-screen h-[80vh] flexCenter">
       <section className="w-80 h-[350px] container flexCol items-center justify-evenly">
-        <Link to="/loginemail">
-          <LoginButton />
-        </Link>
-        <LoginButton />
-        <LoginButton />
-        <LoginButton />
-        <LoginButton />
+        <LoginButton type="Email" />
+        <LoginButton type="Google" />
+        <LoginButton type="Kakao" />
       </section>
       <section className=" ml-6 hidden md:block">
-        <p className="text-7xl bg-blue-200   px-2 py-1 relative">
+        <p className="text-7xl  px-2 py-1 relative">
           쉽고, 간편하게
-          <span className="absolute right-0 top-0 h-full w-[2px] bg-black animate-blink"></span>
+          <span className="absolute right-0 top-0 h-full w-[3px] bg-black animate-blink"></span>
         </p>
         <p className="text-lg bg-red text-slate-500 mt-3 px-2">
           커리어를 향해 나를 이끄는 곳, 마그넷
@@ -26,24 +21,5 @@ const LoginPage = () => {
     </div>
   );
 };
-
-// const Login = () => {
-//   const redirectToGoogleOAuth = () => {
-//     window.location.href =
-//       'https://accounts.google.com/o/oauth2/auth?' +
-//       'client_id=55616367009-acbdl0au5jp7o5m4f09thkt3dn9ies32.apps.googleusercontent.com&' +
-//       'redirect_uri=http://localhost:3000/login&' +
-//       'response_type=code&' +
-//       'scope=https://www.googleapis.com/auth/userinfo.email';
-//   };
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         {/* 버튼을 클릭하면 Google OAuth로 리디렉션 */}
-//         <button onClick={redirectToGoogleOAuth}>Google 로그인</button>
-//       </header>
-//     </div>
-//   );
-// };
 
 export default LoginPage;
