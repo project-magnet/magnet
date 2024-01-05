2437af200c8e54370dd2386d9c540f3ea5646ed0
qpwoei0123 <qpwoei0123@naver.com>
2024-01-05 14:02:19 +0900

    feat: 스크롤 위치 초기화 함수 구현

--- docs/src/index.tsx
+++ docs/src/index.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'; // * BrowserRouter 불러오기
 import App from './App';
 import './styles/tailwind.css';
+import ScrollToTop from './utils/common/scrollToTop';
 
 // * App 을 BrowserRouter 로 감싸기
 ReactDOM.render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>,
   document.getElementById('root'),
