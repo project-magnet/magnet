eb0ba5bea4901ebc6799c139200fda01f890c467
qpwoei0123 <qpwoei0123@naver.com>
2024-01-07 05:39:11 +0900

    design: 반응형 작업

---
+++ docs/src/component/payment/PaymentPopup.tsx
@@ -0,0 +1,10 @@
+const PaymentPopup = () => {
+  return (
+    <div className="flexCenter w-screen h-screen fixed  border bg-opacity-30 bg-black z-10 top-0  left-0">
+      <section className="bg-white w-screen  mx-10 md:w-[500px] h-4/5 md:h-full border rounded-xl"></section>
+      ;
+    </div>
+  );
+};
+
+export default PaymentPopup;
