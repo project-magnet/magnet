d4a93590030f0d3a6618fcd10594aaec3f46e1e0
qpwoei0123 <qpwoei0123@naver.com>
2024-01-17 15:39:08 +0900

    refactor: 라우팅기능 분리

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,36 +1,16 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import HomePage from './pages/HomePage';
-import LoginEmailPage from './pages/LoginEmailPage';
 import Header from './component/common/Header';
 import BottomMenu from './component/common/BottomMenu';
 import Footer from './component/common/Footer';
-import SignupPage from './pages/SignupPage';
-import Authpage from './pages/AuthPage';
-import MentorPage from './pages/MentorPage';
-import MentorListPage from './pages/MentorListPage';
-import PaymentCompletedPage from './pages/PaymentCompletedPage';
-import UserPage from './pages/UserPage';
+import { Router } from './Router';
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-      <Routes>
-        <Route path="/magnet" Component={HomePage} />
-        <Route path="/login" Component={LoginPage} />
-        <Route path="/loginemail" Component={LoginEmailPage} />
-        <Route path="/signup" Component={SignupPage} />
-        <Route path="/auth" Component={Authpage} />
-        <Route path="/mentor" Component={MentorPage} />
-        <Route path="/mentorlist" Component={MentorListPage} />
-        <Route path="/paymentcompleted" Component={PaymentCompletedPage} />
-        <Route path="/user" Component={UserPage} />
-      </Routes>
+      <Router />
       <BottomMenu />
       <Footer />
-    </div>
+    </>
   );
 }
 
