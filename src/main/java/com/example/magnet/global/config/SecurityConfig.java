a64f7a88ba3fb8d4a31b271767084143ddda6968
Lee dong yeol <dlehdduf0902@naver.com>
2024-02-04 21:23:42 +0900

    [feat]getMentori m 단건 조회기능 구현

--- src/main/java/com/example/magnet/global/config/SecurityConfig.java
+++ src/main/java/com/example/magnet/global/config/SecurityConfig.java
@@ -66,7 +66,8 @@ public class SecurityConfig {
                         .requestMatchers("/mentee/**").hasRole("MENTEE")
                         .requestMatchers("/member/**").hasAnyRole("ADMIN","USER","MENTOR","MENTEE")
                         .requestMatchers("/member/extract").permitAll()
-                        .requestMatchers("/mentoring/create").hasRole("MENTOR")
+                        .requestMatchers("/mentoring/create").hasAnyRole("MENTOR")
+                        .requestMatchers("/mentoring/get/**").permitAll()
                         .anyRequest().authenticated() //그 외 나머지는 인증 완료 후 접근 가능
                 )
                 .with(new CustomFilterConfigurer(), Customizer.withDefaults()) // apply(new CustomFilterConfigurer) 로그인 경로 삽입
