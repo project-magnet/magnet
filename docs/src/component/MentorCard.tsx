19e12361ee1dfe652275cecaf02a937c6f25279e
qpwoei0123 <qpwoei0123@naver.com>
2024-02-05 16:48:39 +0900

    feat: add global states setter

--- docs/src/component/MentorCard.tsx
+++ docs/src/component/MentorCard.tsx
@@ -1,28 +1,26 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import {useEffect} from 'react';
+import PopupStore from '../store/PopupStore';
 
 const MentorCard = () => {
-	return (
-		<Link to="/mentor">
-			<div className="p-5 size-72 boxShadow bg-background flexCol gap-5">
-				<div>
-					<p className="truncate font-bold text-xl">김브라키오사우루스</p>
-					<p className="truncate text-secondary text-sm mt-1">#투썸플레이스</p>
-					<p className="truncate text-secondary text-sm mt-1">#프론트엔드</p>
-					<p className="truncate text-secondary text-sm mt-1">#23년차 시니어</p>
-				</div>
+	const setIsOpenTrue = PopupStore(state => state.setIsOpenTure);
 
-				<div className="line-clamp-5">
-					<p className="">
-						장기근속의 비밀에 대해서 알려드립니다.장기근속의 비밀에 대해서 알려드립니다.장기근속의
-						비밀에 대해서 알려드립니다. 장기근속의 비밀에 대해서 알려드립니다.장기근속의 비밀에
-						대해서 알려드립니다.장기근속의 비밀에 대해서 알려드립니다. 장기근속의 비밀에 대해서
-						알려드립니다.장기근속의 비밀에 대해서 알려드립니다.장기근속의 비밀에 대해서
-						알려드립니다.
-					</p>
-				</div>
+	const handleClick = () => {
+		setIsOpenTrue();
+	};
+
+	return (
+		<div onClick={handleClick} className="p-5 size-72 boxShadow bg-background flexCol gap-5">
+			<div>
+				<p className="truncate font-bold text-xl">김브라키오사우루스</p>
+				<p className="truncate text-secondary text-sm mt-1">#투썸플레이스</p>
+				<p className="truncate text-secondary text-sm mt-1">#프론트엔드</p>
+				<p className="truncate text-secondary text-sm mt-1">#23년차 시니어</p>
 			</div>
-		</Link>
+
+			<div className="line-clamp-5">
+				<p className="">[프론트엔드] 23년차 시니어 개발자의 비밀보따리를 풀어보는시간</p>
+			</div>
+		</div>
 	);
 };
 
