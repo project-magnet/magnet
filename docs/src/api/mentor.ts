d7679758a5b0fa3474d8d2168f974e8433aaa382
qpwoei0123 <qpwoei0123@naver.com>
2024-02-10 05:44:44 +0900

    feat: 멘토 리스트 가져오기 API 추가

--- docs/src/api/mentor.ts
+++ docs/src/api/mentor.ts
@@ -45,15 +45,6 @@ export const createMentor = async (data: createMentorData) => {
 	}
 };
 
-export type MentoringDtoList = {
-	id: number;
-	title: string;
-	content: string;
-	pay: string;
-	period: string;
-	participants: number;
-	category: string;
-};
 export type getMentorData = {
 	mentorId: number;
 	mentorName: string;
@@ -65,6 +56,15 @@ export type getMentorData = {
 	aboutMe: string;
 	github: string;
 	mentoringDtoList: MentoringDtoList[];
+};
+type MentoringDtoList = {
+	id: number;
+	title: string;
+	content: string;
+	pay: string;
+	period: string;
+	participants: number;
+	category: string;
 };
 
 export const getMentor = async (): Promise<getMentorData> => {
@@ -82,6 +82,78 @@ export const getMentor = async (): Promise<getMentorData> => {
 		return response.data;
 	} catch (error) {
 		console.error('멘토정보 불러오기 실패', error);
+		throw error;
+	}
+};
+
+export type getMentorListData = {
+	content: Content[];
+	pageable: Pageable;
+	last: boolean;
+	totalPages: number;
+	totalElements: number;
+	size: number;
+	number: number;
+	sort: Sort2;
+	first: boolean;
+	numberOfElements: number;
+	empty: boolean;
+};
+
+interface Content {
+	mentorId: number;
+	mentorName: string;
+	career: string;
+	field: string;
+	task: string;
+	email: string;
+	phone: string;
+	aboutMe: string;
+	github: string;
+	mentoringId: number;
+	mentoringTitle: string;
+	mentoringContent: string;
+	mentoringPay: string;
+	mentoringPeriod: string;
+	mentoringParticipants: number;
+	mentoringCategory: string;
+}
+
+interface Pageable {
+	pageNumber: number;
+	pageSize: number;
+	sort: Sort;
+	offset: number;
+	paged: boolean;
+	unpaged: boolean;
+}
+
+interface Sort {
+	empty: boolean;
+	sorted: boolean;
+	unsorted: boolean;
+}
+
+interface Sort2 {
+	empty: boolean;
+	sorted: boolean;
+	unsorted: boolean;
+}
+
+export const getMentorList = async (offset: number, size: number): Promise<getMentorListData> => {
+	try {
+		const response = await axios.get<getMentorListData>(
+			`${baseUrl}/mentor/list?offset${offset}&size=${size}`,
+			{
+				headers: {
+					'Content-Type': 'application/json',
+					'ngrok-skip-browser-warning': 'true',
+				},
+			},
+		);
+		return response.data;
+	} catch (error) {
+		console.error('멘토 리스트 불러오기 실패', error);
 		throw error;
 	}
 };
