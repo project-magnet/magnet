bc92dcb411e9ff28c4a7ba11d28047d803ec002f
qpwoei0123 <qpwoei0123@naver.com>
2024-02-10 05:37:16 +0900

    feat: 멘토 가져오기 API 추가

--- docs/src/api/mentor.ts
+++ docs/src/api/mentor.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseUrl = process.env.REACT_APP_BASE_URL || 'NO_BASE_URL';
 
-export type createMentorResponse = {
+export type createMentorData = {
 	mentorName: string;
 	field: string;
 	career: string;
@@ -13,21 +13,21 @@ export type createMentorResponse = {
 	github: string;
 };
 
-export const createMentor = async (body: createMentorResponse) => {
+export const createMentor = async (data: createMentorData) => {
 	try {
 		const authorToken = sessionStorage.getItem('Authorization');
 		const refreshToken = sessionStorage.getItem('RefreshToken');
 		const response = await axios.post(
 			`${baseUrl}/mentor/create`,
 			{
-				mentorName: body.mentorName,
-				field: body.field,
-				career: body.career,
-				task: body.task,
-				email: body.email,
-				phone: body.phone,
-				aboutMe: body.aboutMe,
-				github: body.github,
+				mentorName: data.mentorName,
+				field: data.field,
+				career: data.career,
+				task: data.task,
+				email: data.email,
+				phone: data.phone,
+				aboutMe: data.aboutMe,
+				github: data.github,
 			},
 			{
 				headers: {
@@ -41,6 +41,47 @@ export const createMentor = async (body: createMentorResponse) => {
 		return response.data;
 	} catch (error) {
 		console.error('멘토등록 실패', error);
+		throw error;
+	}
+};
+
+export type MentoringDtoList = {
+	id: number;
+	title: string;
+	content: string;
+	pay: string;
+	period: string;
+	participants: number;
+	category: string;
+};
+export type getMentorData = {
+	mentorId: number;
+	mentorName: string;
+	career: string;
+	field: string;
+	task: string;
+	email: string;
+	phone: string;
+	aboutMe: string;
+	github: string;
+	mentoringDtoList: MentoringDtoList[];
+};
+
+export const getMentor = async (): Promise<getMentorData> => {
+	try {
+		const authorToken = sessionStorage.getItem('Authorization');
+		const refreshToken = sessionStorage.getItem('RefreshToken');
+		const response = await axios.get<getMentorData>(`${baseUrl}/mentor/get`, {
+			headers: {
+				'Content-Type': 'application/json',
+				'ngrok-skip-browser-warning': 'true',
+				Authorization: `${authorToken}`,
+				RefreshToken: `${refreshToken}`,
+			},
+		});
+		return response.data;
+	} catch (error) {
+		console.error('멘토정보 불러오기 실패', error);
 		throw error;
 	}
 };
