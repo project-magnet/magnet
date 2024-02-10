6ce2dceddd29894f50ec0d4bd8a9162f6caca9fc
qpwoei0123 <qpwoei0123@naver.com>
2024-02-11 06:26:54 +0900

    design: 링크 스타일 변경

--- docs/src/component/common/BottomMenu.tsx
+++ docs/src/component/common/BottomMenu.tsx
@@ -1,19 +1,33 @@
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import {useLocation} from 'react-router-dom';
 
 const BottomMenu = () => {
-  return (
-    <section
-      className="flexCenter justify-around fixed bottom-0 h-14 w-full 
+	const location = useLocation();
+	return (
+		<section
+			className="flexCenter justify-around fixed bottom-0 h-14 w-full 
     border-t bg-white sm:hidden "
-    >
-      <Link to="/mentorlist" className="tracking-wide font-semibold">
-        둘러보기
-      </Link>
-      <Link to="/login" className="tracking-wide font-semibold">
-        로그인
-      </Link>
-    </section>
-  );
+		>
+			<Link to="/mentorlist" className="tracking-wide font-semibold">
+				<p
+					className={`${
+						'/mentorlist' === location.pathname ? 'text-additional2' : 'text-black'
+					} transition`}
+				>
+					둘러보기
+				</p>
+			</Link>
+			<Link to="/login" className="tracking-wide font-semibold">
+				<p
+					className={`${
+						'/login' === location.pathname ? 'text-additional2' : 'text-black'
+					} transition`}
+				>
+					로그인
+				</p>
+			</Link>
+		</section>
+	);
 };
 
 export default BottomMenu;
--- docs/src/component/common/Header.tsx
+++ docs/src/component/common/Header.tsx
@@ -1,9 +1,11 @@
 import {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
+import {useLocation} from 'react-router-dom';
 
 const Header = () => {
 	const [isMentor, setIsMentor] = useState(false); // [TODO] 로그인 시 멘토인지 멘티인지 확인하는 로직 필요
 	const token = sessionStorage.getItem('Authorization');
+	const location = useLocation();
 
 	useEffect(() => {
 		const token = sessionStorage.getItem('Authorization');
@@ -18,25 +20,55 @@ const Header = () => {
    bg-white  h-10 sm:h-16 sticky top-0 opacity-95 border-b "
 		>
 			<Link to="/magnet" className="tracking-[5px] font-bold  mx-24 ">
-				MAGNET
+				<p
+					className={`${
+						'/magnet' === location.pathname ? 'text-additional2' : 'text-black'
+					} transition`}
+				>
+					MAGNET
+				</p>
 			</Link>
-			<Link to="/mentorlist" className="tracking-wide hidden sm:block mr-10">
-				둘러보기
+			<Link to="/mentorlist" className={`tracking-wide hidden sm:block mr-10`}>
+				<p
+					className={`${
+						'/mentorlist' === location.pathname ? 'text-additional2' : 'text-black'
+					} transition`}
+				>
+					둘러보기
+				</p>
 			</Link>
 			{token ? (
 				<>
 					<Link to="/user" className="tracking-wide hidden sm:block mr-10">
-						마이페이지
+						<p
+							className={`${
+								'/user' === location.pathname ? 'text-additional2' : 'text-black'
+							} transition`}
+						>
+							마이페이지
+						</p>
 					</Link>
 					{isMentor && (
 						<Link to="/creatementoring" className="tracking-wide hidden sm:block">
-							멘토링 개설하기
+							<p
+								className={`${
+									'/creatementoring' === location.pathname ? 'text-additional2' : 'text-black'
+								} transition`}
+							>
+								멘토링 개설하기
+							</p>
 						</Link>
 					)}
 				</>
 			) : (
-				<Link to="/login" className="tracking-wide hidden sm:block mr-10">
-					로그인
+				<Link to="/login" className="tracking-wide hidden sm:block mr-10 ">
+					<p
+						className={`${
+							'/login' === location.pathname ? 'text-additional2' : 'text-black'
+						} transition`}
+					>
+						로그인
+					</p>
 				</Link>
 			)}
 		</header>
