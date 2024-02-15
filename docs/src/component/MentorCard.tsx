378f9a9af40a07d487c147e9d7f1f2e6be7c13ff
qpwoei0123 <qpwoei0123@naver.com>
2024-02-15 10:05:04 +0900

    feat: Props 추가 및 데이터 출력 변경

--- docs/src/component/MentorCard.tsx
+++ docs/src/component/MentorCard.tsx
@@ -1,7 +1,18 @@
 import {useEffect} from 'react';
 import PopupStore from '../store/PopupStore';
 
-const MentorCard = () => {
+interface MyComponentProps {
+	data: {
+		mentoringTitle: string;
+		mentoringContent: string;
+		mentorName: string;
+		career: string;
+		field: string;
+		task: string;
+	};
+}
+
+const MentorCard = (data: MyComponentProps) => {
 	const setIsOpenTrue = PopupStore(state => state.setIsOpenTure);
 
 	const handleClick = () => {
@@ -14,14 +25,14 @@ const MentorCard = () => {
 			className="p-5 buttonStyle size-72 cursor-pointer bg-background flexCol gap-5"
 		>
 			<div>
-				<p className="truncate font-bold text-xl">김브라키오사우루스</p>
-				<p className="truncate text-secondary text-sm mt-1">#투썸플레이스</p>
-				<p className="truncate text-secondary text-sm mt-1">#프론트엔드</p>
-				<p className="truncate text-secondary text-sm mt-1">#23년차 시니어</p>
+				<p className="truncate font-bold text-xl">{data.data.mentorName}</p>
+				<p className="truncate text-secondary text-sm mt-1">{`#${data.data.task}`}</p>
+				<p className="truncate text-secondary text-sm mt-1">{`#${data.data.field}`}</p>
+				<p className="truncate text-secondary text-sm mt-1">{`#${data.data.career}`}</p>
 			</div>
 
 			<div className="line-clamp-5">
-				<p className="">[프론트엔드] 23년차 시니어 개발자의 비밀보따리를 풀어보는시간</p>
+				<p className="">{data.data.mentoringTitle}</p>
 			</div>
 		</div>
 	);
