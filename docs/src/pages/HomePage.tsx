ca3efb648c5115fbff3467ec2036c21d6113b6b5
qpwoei0123 <qpwoei0123@naver.com>
2024-03-05 00:29:52 +0900

    design: 렌딩페이지 디자인 개선

--- docs/src/pages/HomePage.tsx
+++ docs/src/pages/HomePage.tsx
@@ -1,29 +1,52 @@
+import {useNavigate} from 'react-router-dom';
 import MentorCard from '../component/MentorCard';
-import {Link} from 'react-router-dom';
 
 const HomePage = () => {
+	const navigate = useNavigate();
+	const handleClick = () => {
+		navigate(`/mentorlist`);
+	};
+
 	return (
 		<section>
-			<section className="py-32 ">
-				<div className="flexCol gap-4 ml-5 sm:ml-20 text-primary">
-					<p className="text-3xl font-bold tracking-tighter  sm:text-6xl">Connect with Magnet</p>
-					<p className=" text-secondary text-xs sm:text-xl">
-						당신의 커리어 여정을 안내해 줄 완벽한 멘토를 찾아보세요.
+			<section className=" py-20 ">
+				<div className="flexCenter size-full flex-col ">
+					<p className="font-PartialSansKR_Regular text-[3rem]  sm:text-[6rem] md:text-[8rem] lg:text-[10rem]">
+						Connect <br />
+						with
+					</p>
+					<p className="font-PartialSansKR_Regular text-[3rem]  text-additional2 sm:text-[6rem] md:text-[8rem] lg:text-[10rem]">
+						Magnet !
 					</p>
 				</div>
 			</section>
 
-			<section className="py-12 bg-slate-100 flexCol justify-center items-center gap-10">
-				<div className="flexCol items-center gap-5 ">
-					<h2 className="text-3xl font-bold sm:text-5xl tracking-tighter text-primary">
-						Meet our Mentors
-					</h2>
-					<p className=" text-secondary text-xl">멘티의 성공을 위해 노력하는 멘토를 만나보세요.</p>
+			<section className="flexCenter justify-around  bg-additional2 py-20 ">
+				<div className="flexCol hidden items-start gap-5 sm:block">
+					<h2 className="font-SEBANG_Gothic_Bold text-2xl ">Meet our Mentors</h2>
+					<p className="font-SEBANG_Gothic_Bold  text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
+						마그넷의 멘토를 만나보세요.
+					</p>
 				</div>
-				<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5"></div>
-				<Link to="/mentorlist">
-					<button className="text-secondary font-bold">더 알아보기</button>
-				</Link>
+
+				{/* 카드 */}
+				<div
+					onClick={handleClick}
+					className="buttonStyle flexCol size-72 animate-fadeIn cursor-pointer gap-5 bg-background p-5"
+				>
+					<div>
+						<p className="truncate text-xl font-bold">마그넷과 함께</p>
+						<p className="mt-1 truncate text-sm text-secondary">{`#각 분야의`}</p>
+						<p className="mt-1 truncate text-sm text-secondary">{`#최고의 멘토와`}</p>
+						<p className="mt-1 truncate text-sm text-secondary">{`#만나 보세요!`}</p>
+					</div>
+					<div className="h-0.5 w-full border border-dashed" />
+					<div className="flexCenter size-full">
+						<p className="font-SEBANG_Gothic_Bold animate-bounce text-3xl text-additional2">
+							더 알아보기!
+						</p>
+					</div>
+				</div>
 			</section>
 		</section>
 	);
