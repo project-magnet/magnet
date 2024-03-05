a78261be83808dbf0bbe08f08c124d37cd7e228b
qpwoei0123 <qpwoei0123@naver.com>
2024-03-05 12:58:05 +0900

    design: 홈페이지 애니메이션 추가

--- docs/src/pages/HomePage.tsx
+++ docs/src/pages/HomePage.tsx
@@ -1,5 +1,4 @@
 import {useNavigate} from 'react-router-dom';
-import MentorCard from '../component/MentorCard';
 
 const HomePage = () => {
 	const navigate = useNavigate();
@@ -9,20 +8,27 @@ const HomePage = () => {
 
 	return (
 		<section>
-			<section className=" py-20 ">
-				<div className="flexCenter size-full flex-col ">
-					<p className="font-PartialSansKR_Regular text-[3rem]  sm:text-[6rem] md:text-[8rem] lg:text-[10rem]">
+			<section className=" py-20">
+				<div
+					className="flexCenter size-full flex-col 
+				text-[3rem] sm:text-[6rem] md:text-[8rem] lg:text-[10rem]"
+				>
+					<p className="font-PartialSansKR_Regular">
 						Connect <br />
 						with
 					</p>
-					<p className="font-PartialSansKR_Regular text-[3rem]  text-additional2 sm:text-[6rem] md:text-[8rem] lg:text-[10rem]">
-						Magnet !
-					</p>
+					<div className="flex text-additional2 *:font-PartialSansKR_Regular">
+						{['M', 'a', 'g', 'n', 'e', 't', '!'].map((el, index) => (
+							<p className="hoverAnimationUpDown" key={index}>
+								{el}
+							</p>
+						))}
+					</div>
 				</div>
 			</section>
 
-			<section className="flexCenter justify-around  bg-additional2 py-20 ">
-				<div className="flexCol hidden items-start gap-5 sm:block">
+			<section className="flexCenter justify-evenly  bg-additional2 py-20 ">
+				<div className="flexCol hidden items-start gap-5 md:block">
 					<h2 className="font-SEBANG_Gothic_Bold text-2xl ">Meet our Mentors</h2>
 					<p className="font-SEBANG_Gothic_Bold  text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
 						마그넷의 멘토를 만나보세요.
@@ -42,7 +48,7 @@ const HomePage = () => {
 					</div>
 					<div className="h-0.5 w-full border border-dashed" />
 					<div className="flexCenter size-full">
-						<p className="font-SEBANG_Gothic_Bold animate-bounce text-3xl text-additional2">
+						<p className="animate-bounce font-SEBANG_Gothic_Bold text-3xl text-additional2">
 							더 알아보기!
 						</p>
 					</div>
