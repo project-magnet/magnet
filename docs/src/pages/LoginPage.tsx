cbbf0b317a958955a21819fd34f9c3849d816a3c
qpwoei0123 <qpwoei0123@naver.com>
2024-03-05 12:55:56 +0900

    design: 폰트변경

--- docs/src/pages/LoginPage.tsx
+++ docs/src/pages/LoginPage.tsx
@@ -13,8 +13,8 @@ const LoginPage = () => {
 	};
 
 	return (
-		<div className="w-full h-[80vh] flexCenter">
-			<section className="w-80 h-[350px] container flexCol items-center justify-evenly">
+		<div className="flexCenter h-[80vh] w-full">
+			<section className="flexCol container h-[350px] w-80 items-center justify-evenly">
 				<LoginButton type="Google" />
 				<LoginButton type="Kakao" />
 				<LoginButton type="Naver" />
@@ -24,11 +24,11 @@ const LoginPage = () => {
 				</button>
 			</section>
 			<section className=" ml-6 hidden md:block">
-				<p className="text-7xl  px-2 py-1 relative">
+				<p className="relative  px-2 py-1 text-7xl font-black">
 					쉽고, 간편하게
-					<span className="absolute right-0 top-0 h-full w-[3px] bg-black animate-blink"></span>
+					<span className="absolute right-0 top-0 h-full w-[3px] animate-blink bg-black"></span>
 				</p>
-				<p className="text-lg bg-red text-slate-500 mt-3 px-2">
+				<p className="bg-red mt-3 px-2 text-lg text-slate-500">
 					커리어를 향해 나를 이끄는 곳, 마그넷
 				</p>
 			</section>
