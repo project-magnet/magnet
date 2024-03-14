174cdd52a7ea242b8a0836e3dd7dac10110ee8f5
qpwoei0123 <qpwoei0123@naver.com>
2024-03-14 22:12:11 +0900

    feat: 로그인 팝업 전역으로 설정

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,17 +1,21 @@
 import Header from './component/common/Header';
 import BottomMenu from './component/common/BottomMenu';
 import Footer from './component/common/Footer';
-import { Router } from './Router';
+import {LoginPopup} from './component/auth/LoginPopup';
+import {LoginPopupStore} from './store/LoginPopupStore';
+import {Router} from './Router';
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Router />
-      <BottomMenu />
-      <Footer />
-    </>
-  );
+	const isLoginPopupOpen = LoginPopupStore(state => state.loginPopupIsOpen);
+	return (
+		<>
+			{isLoginPopupOpen && <LoginPopup />}
+			<Header />
+			<Router />
+			<BottomMenu />
+			<Footer />
+		</>
+	);
 }
 
 export default App;
