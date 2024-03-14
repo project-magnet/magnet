5c5dfd3b3127445f07c47e231c72234516dd72a8
qpwoei0123 <qpwoei0123@naver.com>
2024-03-14 15:36:16 +0900

    design: tickle 애니메이션 추가

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -25,6 +25,7 @@ export const theme = {
 			shake: 'shake 0.3s ease-in-out forwards',
 			showSideRight: `showSideRight 1s ${cubric} forwards`,
 			fadeInMoveDown: `fadeInMoveDown 1s ${cubric} forwards`,
+			tickle: `tickle 0.2s ${cubric} forwards`,
 		},
 		keyframes: {
 			blink: {
@@ -49,13 +50,19 @@ export const theme = {
 				'20%, 40%, 60%, 80%': {transform: 'translateX(1px)'},
 			},
 			showSideRight: {
-				'0%': {transform: 'translateX(100%)'},
+				'0%': {opacity: 0, transform: 'translateX(-100%)'},
 
-				'100%': {transform: 'translateX(0%)'},
+				'100%': {opacity: 1, transform: 'translateX(0%)'},
 			},
 			fadeInMoveDown: {
 				'0%': {opacity: 0, transform: 'translateY(-100%)'},
 				'100%': {opacity: 1, transform: 'translateY(0%)'},
+			},
+			tickle: {
+				'0%': {transform: 'rotate(0deg)'},
+				'30%': {transform: 'rotate(5deg)'},
+				'70%': {transform: 'rotate(-5deg)'},
+				'100%': {transform: 'rotate(0deg)'},
 			},
 		},
 		fontSize: {
