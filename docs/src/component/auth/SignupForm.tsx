bd010b1eeb36aa60a7255632df3742070847f7a2
qpwoei0123 <qpwoei0123@naver.com>
2024-03-19 19:53:52 +0900

    refactor: 디자인 개선 및 코드 중복 개선

--- docs/src/component/auth/SignupForm.tsx
+++ docs/src/component/auth/SignupForm.tsx
@@ -1,8 +1,12 @@
-import {useState, useEffect} from 'react';
-import {Link, useNavigate} from 'react-router-dom';
+import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 import {signup} from '../../api/auth';
+import {WarningMessage} from '../common/WarningMessage';
+import {LoginPopupStore} from '../../store/LoginPopupStore';
 
 const SignupForm = () => {
+	const navigate = useNavigate();
+	const setLoginPopupIsOpenTrue = LoginPopupStore(state => state.setLoginPopupIsOpenTrue);
 	const [form, setForm] = useState({
 		email: '',
 		username: '',
@@ -10,207 +14,154 @@ const SignupForm = () => {
 		nickName: '',
 		phone: '',
 		addressDto: {city: 'city', street: 'street'},
-		confirmPassword: '',
 	});
 	// 유효성 검사
 	const [checkForm, setCheckForm] = useState({
 		email: {focused: false, validated: false},
 		username: {focused: false, validated: false},
 		password: {focused: false, validated: false},
-		confirmPassword: {focused: false, validated: false},
 		nickName: {focused: false, validated: false},
 		phone: {focused: false, validated: false},
 	});
-
-	const navigate = useNavigate();
+	// 유효성 검사 정규식
+	const validators = {
+		email: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/,
+		username: /^[가-힣]{2,}$/,
+		nickName: /^[A-Za-z0-9-가-힣]{3,}$/,
+		phone: /^([0-9]{2}|[0-9]{3})-([0-9]{3,4})-([0-9]{4})$/,
+		password: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+	};
 
 	const handleSignup = async () => {
-		const fetchSignup = async () => {
-			try {
-				const {confirmPassword, ...newForm} = form;
-				await signup(newForm);
-				navigate('/loginemail');
-			} catch (e) {
-				console.error('회원가입에 실패했습니다.', e);
-			}
-		};
-		fetchSignup();
-	};
-	// 이하 검증 함수.
-	const validateEmail = (vlaue: string) => {
-		setForm({...form, email: vlaue});
-		if (/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/.test(vlaue)) {
-			setCheckForm({...checkForm, email: {...checkForm.email, validated: true}});
-		} else {
-			setCheckForm({...checkForm, email: {...checkForm.email, validated: false}});
+		try {
+			await signup(form);
+			navigate('/magnet');
+			setLoginPopupIsOpenTrue();
+		} catch (e) {
+			console.error('회원가입에 실패했습니다.', e);
 		}
 	};
 
-	const validateusername = (value: string) => {
-		setForm({...form, username: value});
-		if (/^[가-힣]{2,}$/.test(value)) {
-			setCheckForm({...checkForm, username: {...checkForm.username, validated: true}});
-		} else {
-			setCheckForm({...checkForm, username: {...checkForm.username, validated: false}});
-		}
-	};
-
-	const validateNickName = (value: string) => {
-		setForm({...form, nickName: value});
-		if (/^[A-Za-z0-9-가-힣]{3,}$/.test(value)) {
-			setCheckForm({...checkForm, nickName: {...checkForm.nickName, validated: true}});
-		} else {
-			setCheckForm({...checkForm, nickName: {...checkForm.nickName, validated: false}});
-		}
-	};
-	const validatePhone = (value: string) => {
-		setForm({...form, phone: value});
-		// 숫자와 하이픈 허용하고 10자리 이상
-		if (/^([0-9]{2}|[0-9]{3})-([0-9]{3,4})-([0-9]{4})$/.test(value)) {
-			setCheckForm({...checkForm, phone: {...checkForm.phone, validated: true}});
-		} else {
-			setCheckForm({...checkForm, phone: {...checkForm.phone, validated: false}});
-		}
-	};
-
-	const validatePassword = (value: string) => {
+	type valiateType = 'email' | 'username' | 'nickName' | 'phone' | 'password';
+	const valiateField = (field: valiateType, value: string) => {
+		setForm({...form, [field]: value});
 		setCheckForm(prevState => {
-			if (/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(value)) {
-				return {...prevState, password: {...prevState.password, validated: true}};
-			} else {
-				return {...prevState, password: {...prevState.password, validated: false}};
-			}
+			return validators[field].test(String(value))
+				? {...prevState, [field]: {...prevState[field], validated: true}}
+				: {...prevState, [field]: {...prevState[field], validated: false}};
 		});
 	};
-
-	const validateConfirmPassword = (value: string) => {
-		setCheckForm(prevState => {
-			if (value === form.password) {
-				return {
-					...prevState,
-					confirmPassword: {...prevState.confirmPassword, validated: true},
-				};
-			} else {
-				return {
-					...prevState,
-					confirmPassword: {...prevState.confirmPassword, validated: false},
-				};
-			}
-		});
-	};
-
-	// 패스워드는 입력이 서로 다를 때마다 검증을 해야 한다.
-	useEffect(() => {
-		const validateAndSetPassword = () => {
-			validatePassword(form.password);
-			validateConfirmPassword(form.confirmPassword);
-		};
-
-		validateAndSetPassword();
-	}, [form.password, form.confirmPassword]);
 
 	return (
-		<section className="inputStyle flexCol items-center gap-5">
-			<div>
-				<p>이메일</p>
-				<input
-					placeholder="이메일을 입력하세요."
-					value={form.email}
-					onChange={e => validateEmail(e.target.value)}
-					onFocus={() => setCheckForm({...checkForm, email: {...checkForm.email, focused: true}})}
-				/>
-				{!checkForm.email.validated && checkForm.email.focused && (
-					<p className="warning">이메일 형식이 올바르지 않습니다.</p>
-				)}
-			</div>
-
-			<div>
-				<p>이름</p>
-				<input
-					placeholder="이름을 입력하세요."
-					value={form.username}
-					onChange={e => validateusername(e.target.value)}
-					onFocus={() =>
-						setCheckForm({...checkForm, username: {...checkForm.username, focused: true}})
-					}
-				/>
-				{!checkForm.username.validated && checkForm.username.focused && (
-					<p className="warning">2자리 이상의 한글이여야 합니다.</p>
-				)}
-			</div>
-
-			<div>
-				<p>닉네임</p>
-				<input
-					placeholder="닉네임을 입력하세요."
-					value={form.nickName}
-					onChange={e => validateNickName(e.target.value)}
-					onFocus={() =>
-						setCheckForm({...checkForm, nickName: {...checkForm.nickName, focused: true}})
-					}
-				/>
-				{!checkForm.nickName.validated && checkForm.nickName.focused && (
-					<p className="warning">특수문자 제외 3자리 이상이여야 합니다.</p>
-				)}
-			</div>
-
-			<div>
-				<p>전화번호</p>
-				<input
-					placeholder="전화번호를 입력하세요."
-					value={form.phone}
-					onChange={e => validatePhone(e.target.value)}
-					onFocus={() => setCheckForm({...checkForm, phone: {...checkForm.phone, focused: true}})}
-				/>
-				{!checkForm.phone.validated && checkForm.phone.focused && (
-					<p className="warning">하이픈(-)을 포함한 9자리 이상 정수여야 합니다.</p>
-				)}
-			</div>
-			<div>
-				<div>
-					<p>비밀번호</p>
+		<section className="flexCol w-full items-center gap-10">
+			<div className="flexCol  w-11/12 md:w-96">
+				<div className={`flexCenter w-full gap-2 border-b-2 p-3  focus-within:border-additional2 `}>
+					<i className={`ri-mail-line ri-lg ${form.email ? 'text-black' : 'text-slate-400'}`} />
 					<input
+						className="flex-grow text-xs outline-none "
+						placeholder="이메일"
+						value={form.email}
+						onChange={e => valiateField('email', e.target.value)}
+						onFocus={() => setCheckForm({...checkForm, email: {...checkForm.email, focused: true}})}
+					/>
+				</div>
+				{checkForm.email.focused && (
+					<WarningMessage
+						message="이메일 형식이어야 합니다."
+						isSuccess={checkForm.email.validated}
+					/>
+				)}
+				<div className="flexCenter w-full gap-2 border-b-2   p-3 focus-within:border-additional2 ">
+					<i className={`ri-key-2-line ri-lg ${form.password ? 'text-black' : 'text-slate-400'}`} />
+					<input
+						className="flex-grow text-xs outline-none"
 						type="password"
-						placeholder="비밀번호를 입력하세요."
+						placeholder="비밀번호"
 						value={form.password}
-						onChange={e => setForm({...form, password: e.target.value})}
+						onChange={e => valiateField('password', e.target.value)}
 						onFocus={() =>
 							setCheckForm({...checkForm, password: {...checkForm.password, focused: true}})
 						}
 					/>
-					{!checkForm.password.validated && checkForm.password.focused && (
-						<p className="warning">최소 8자 이상, 영문과 숫자를 모두 포함해야 합니다.</p>
-					)}
 				</div>
-				<input
-					className="mt-2"
-					type="password"
-					placeholder="비밀번호를 다시 입력하세요."
-					value={form.confirmPassword}
-					onChange={e => setForm({...form, confirmPassword: e.target.value})}
-				/>
-				{checkForm.password.validated && (
-					<p className={`${checkForm.confirmPassword.validated ? 'success' : 'warning'}`}>
-						{checkForm.confirmPassword.validated
-							? '비밀번호가 일치합니다.'
-							: '비밀번호가 일치하지 않습니다.'}
-					</p>
+				{checkForm.password.focused && (
+					<WarningMessage
+						message="최소 8자 이상, 영문과 숫자를 모두 포함해야 합니다."
+						isSuccess={checkForm.password.validated}
+					/>
+				)}
+			</div>
+
+			<div className="flexCol  w-11/12  md:w-96">
+				<div className="flexCenter w-full gap-2 border-b-2  p-3 focus-within:border-additional2 ">
+					<i className={`ri-user-line ri-lg ${form.username ? 'text-black' : 'text-slate-400'}`} />
+					<input
+						className="flex-grow text-xs outline-none"
+						placeholder="실명"
+						value={form.username}
+						onChange={e => valiateField('username', e.target.value)}
+						onFocus={() =>
+							setCheckForm({...checkForm, username: {...checkForm.username, focused: true}})
+						}
+					/>
+				</div>
+				{checkForm.username.focused && (
+					<WarningMessage
+						message="한글 2자리 이상이여야 합니다."
+						isSuccess={checkForm.username.validated}
+					/>
+				)}
+				<div className="flexCenter w-full gap-2 border-b-2  p-3 focus-within:border-additional2 ">
+					<i className={`ri-phone-line ri-lg ${form.phone ? 'text-black' : 'text-slate-400'}`} />
+					<input
+						className="flex-grow text-xs outline-none"
+						placeholder="전화번호"
+						value={form.phone}
+						onChange={e => valiateField('phone', e.target.value)}
+						onFocus={() => setCheckForm({...checkForm, phone: {...checkForm.phone, focused: true}})}
+					/>
+				</div>
+				{checkForm.phone.focused && (
+					<WarningMessage
+						message="하이픈(-)을 포함한 9자리 이상 정수여야 합니다."
+						isSuccess={checkForm.phone.validated}
+					/>
+				)}
+			</div>
+
+			<div className="flexCol  w-11/12  md:w-96">
+				<div className="flexCenter w-full gap-2 border-b-2  p-3 focus-within:border-additional2 ">
+					<i
+						className={`ri-aliens-line ri-lg ${form.nickName ? 'text-black' : 'text-slate-400'}`}
+					/>
+					<input
+						className="flex-grow text-xs outline-none"
+						placeholder="닉네임"
+						value={form.nickName}
+						onChange={e => valiateField('nickName', e.target.value)}
+						onFocus={() =>
+							setCheckForm({...checkForm, nickName: {...checkForm.nickName, focused: true}})
+						}
+					/>
+				</div>
+				{checkForm.nickName.focused && (
+					<WarningMessage
+						message={'특수문자 제외 3자리 이상이여야 합니다.'}
+						isSuccess={checkForm.nickName.validated}
+					/>
 				)}
 			</div>
 
 			{/* 모든 validated가 참인가? */}
 			<button
 				onClick={handleSignup}
-				className={`text-white bg-primary ${
+				className={`buttonStylePrimary w-11/12 md:w-96 ${
 					!Object.values(checkForm).every(field => field.validated) && 'bg-opacity-20'
 				}`}
 				disabled={!Object.values(checkForm).every(field => field.validated)}
 			>
 				회원가입
 			</button>
-			<Link to="/loginemail">
-				<p className="text-sm text-slate-400">로그인으로 돌아가기</p>
-			</Link>
 		</section>
 	);
 };
