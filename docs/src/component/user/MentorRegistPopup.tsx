696ce9ff596c8c77fccd44d87df9e7a6914b0eab
qpwoei0123 <qpwoei0123@naver.com>
2024-03-21 02:09:34 +0900

    refactor: 전역변수 연결 및 디자인 개선

--- docs/src/component/user/MentorRegistPopup.tsx
+++ docs/src/component/user/MentorRegistPopup.tsx
@@ -2,23 +2,26 @@ import 'remixicon/fonts/remixicon.css';
 import {useEffect, useState} from 'react';
 import PopupStore from '../../store/PopupStore';
 import {createMentor} from '../../api/mentor';
+import {MemberStore} from '../../store/MemberStore';
+import {LogoMagnet} from '../common/LogoMagnet';
 
 export const MentorRegistPopup = () => {
 	const setIsOpenFalse = PopupStore(state => state.setIsOpenFalse);
+	const globalMember = MemberStore(state => state.globalMember);
+
+	const [areAllInputsSelected, setAreAllInputsSelected] = useState(false);
 	const [form, setForm] = useState({
-		mentorName: 'no name',
+		mentorName: globalMember.username,
 		field: '',
 		career: '',
 		task: '',
-		email: 'no email',
-		phone: 'no phone',
+		email: globalMember.email,
+		phone: globalMember.phone,
 		aboutMe: '',
 		github: 'no github',
 	});
-	const [areAllInputsSelected, setAreAllInputsSelected] = useState(false);
 
 	const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
-		// 팝업 영역 자체를 클릭한 경우에만 팝업을 닫습니다.
 		e.stopPropagation();
 		if (e.target === e.currentTarget) {
 			setIsOpenFalse();
@@ -46,69 +49,76 @@ export const MentorRegistPopup = () => {
 	return (
 		<div
 			onClick={handleClick}
-			className="flexCenter w-full h-full fixed bg-opacity-30 bg-black z-10 top-0"
+			className="flexCenter fixed top-0 z-20 size-full bg-black bg-opacity-30 backdrop-blur-sm"
 		>
-			<section className="flexCol w-full h-full mt-40 p-0 sm:p-10 sm:mt-0 sm:h-fit sm:w-fit bg-white rounded-xl z-20 gap-5 overflow-y-auto">
-				<div className="flex justify-end items-center">
+			<section className="flexCenter relative w-11/12 flex-col gap-5 rounded-md bg-white p-10 md:w-fit md:px-20 ">
+				<div className="absolute right-1 top-1">
 					<i
 						onClick={handleClick}
-						className="ri-close-line text-2xl cursor-pointer text-slate-400"
+						className="ri-close-line cursor-pointer text-2xl  text-slate-400 transition-colors duration-300 hover:text-black"
 					></i>
 				</div>
-				<div className="w-full flex justify-center">
-					<p className="text-2xl font-semibold">직무 정보</p>
-				</div>
-				<div className="flexCol  gap-5 inputStyle">
-					<div>
-						<p>경력</p>
+				<LogoMagnet word="MAGNET" />
+				<div className="flexCol w-full gap-5 md:w-96">
+					<div className="flexCenter w-full gap-2 border-b-2   p-3 focus-within:border-additional2 ">
+						<i className={`ri-key-2-line ri-lg ${form.field ? 'text-black' : 'text-slate-400'}`} />
 						<select
-							className="border w-full p-2 rounded-md"
+							className="flex-grow text-xs outline-none"
 							value={form.field}
 							onChange={e => setForm({...form, field: e.target.value})}
 						>
-							<option value="">선택하세요</option>
+							<option value="">분야</option>
 							<option value="개발">개발</option>
 							<option value="영업">영업</option>
 							<option value="디자인">디자인</option>
 						</select>
 					</div>
-					<div>
-						<p>경력 레벨</p>
+
+					<div className="flexCenter w-full gap-2 border-b-2   p-3 focus-within:border-additional2 ">
+						<i className={`ri-key-2-line ri-lg ${form.career ? 'text-black' : 'text-slate-400'}`} />
 						<select
-							className="border w-full p-2 rounded-md"
+							className="flex-grow text-xs outline-none"
 							value={form.career}
 							onChange={e => setForm({...form, career: e.target.value})}
 						>
-							<option value="">선택하세요</option>
+							<option value="">경력 레벨</option>
 							<option value="1~3년 주니어">1~3년 주니어</option>
 							<option value="4~8년 미들">4~8년 미들</option>
 							<option value="9년 이상 시니어">9년 이상 시니어</option>
 						</select>
 					</div>
-					<div>
-						<p>현직 회사</p>
-						<input value={form.task} onChange={e => setForm({...form, task: e.target.value})} />
-					</div>
-					<div>
-						<p>자기 소개</p>
+
+					<div className="flexCenter w-full gap-2 border-b-2   p-3 focus-within:border-additional2 ">
+						<i className={`ri-key-2-line ri-lg ${form.task ? 'text-black' : 'text-slate-400'}`} />
 						<input
+							className="flex-grow text-xs outline-none"
+							placeholder="현직"
+							value={form.task}
+							onChange={e => setForm({...form, task: e.target.value})}
+						/>
+					</div>
+					<div className="flexCenter w-full gap-2 border-b-2   p-3 focus-within:border-additional2 ">
+						<i
+							className={`ri-key-2-line ri-lg ${form.aboutMe ? 'text-black' : 'text-slate-400'}`}
+						/>
+						<input
+							className="flex-grow text-xs outline-none"
+							placeholder="자기소개"
 							value={form.aboutMe}
 							onChange={e => setForm({...form, aboutMe: e.target.value})}
 						/>
 					</div>
 				</div>
 
-				<div className="flex justify-center">
-					<button
-						onClick={areAllInputsSelected ? handleSubmit : () => {}}
-						className={`buttonStyle py-2 px-6 text-white ${
-							areAllInputsSelected ? 'bg-black' : 'bg-gray-300'
-						}`}
-						disabled={!areAllInputsSelected}
-					>
-						등록하기
-					</button>
-				</div>
+				<button
+					onClick={areAllInputsSelected ? handleSubmit : () => {}}
+					className={`buttonStylePrimary w-11/12 md:w-96 ${
+						areAllInputsSelected ? 'bg-black' : 'bg-gray-300'
+					}`}
+					disabled={!areAllInputsSelected}
+				>
+					등록하기
+				</button>
 			</section>
 		</div>
 	);
