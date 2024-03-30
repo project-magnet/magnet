55445ee7f75c979a89a74c620a7922f69d749d92
qpwoei0123 <qpwoei0123@naver.com>
2024-03-30 11:39:31 +0900

    chore: Footer 간소화

--- client/src/component/common/Footer.tsx
+++ client/src/component/common/Footer.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
 const Footer = () => {
-  return (
-    <footer className="flexCol py-6 items-center  border-t mb-14 sm:mb-0">
-      <p className="text-xs text-gray-500 dark:text-gray-400">© project magnet</p>
-    </footer>
-  );
+	return (
+		<footer className="flexCol mb-14 items-center  border-t py-6 sm:mb-0">
+			<p className="text-xs text-gray-500 dark:text-gray-400">© project magnet</p>
+		</footer>
+	);
 };
 
 export default Footer;
