12e465955defc61463ee349424b6916824ee859a
qpwoei0123 <qpwoei0123@naver.com>
2024-04-13 13:48:16 +0900

    test: .env 인식문제 테스트

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -26,24 +26,18 @@ const HomePage = () => {
 		},
 		{title: 'AI', id: 'AI', icon: 'robot-3-line'},
 	];
+	const onclick = () => {
+		console.log(process.env.REACT_APP_BASE_URL, 'process.env.REACT_APP_BASE_URL');
+		console.log(process.env.CLIENT_ID, 'process.env.CLIENT_ID');
+	};
 
 	return (
 		<div className="flexCol h-pageRoot justify-center gap-10 overflow-hidden lg:flex-row ">
-			<section className="flexCenter animate-showSideRight flex-col">
-				<div className="flexCenter hidden  flex-col sm:text-[6rem] lg:block 2xl:text-[11rem]  ">
-					<p className="font-PartialSansKR_Regular">Connecting</p>
-					<p className="font-PartialSansKR_Regular">With</p>
-					<div className="flex text-additional2 ">
-						{['M', 'A', 'G', 'N', 'E', 'T', '!'].map((el, index) => (
-							<span className="hoverAnimationUpDown font-PartialSansKR_Regular" key={index}>
-								{el}
-							</span>
-						))}
-					</div>
-				</div>
-
-				<div className="text-[3rem] font-bold sm:text-[4rem] lg:hidden ">
-					<p className="">모두를 위한</p>
+			<section className="flexCenter animate-showSideRight flex-col  font-PartialSansKR_Regular">
+				<div className="text-[3rem] sm:text-[4rem] lg:text-[6rem] 2xl:text-[10rem]">
+					<p className="" onClick={onclick}>
+						모두를 위한
+					</p>
 					<p className="">멘토링 서비스</p>
 					<div className="flex text-additional2">
 						{['마', '그', '넷', '!'].map((el, index) => (
@@ -56,7 +50,7 @@ const HomePage = () => {
 			</section>
 
 			<section className=" noScroll flexCenter h-fit w-screen animate-Tilting overflow-scroll bg-white shadow-md transition duration-300 hover:shadow-xl sm:bottom-10 lg:bottom-0 lg:right-32 lg:h-screen lg:w-fit ">
-				<div className="lg:flexCol flex animate-translateX gap-0 py-3 sm:gap-5 lg:animate-translateY lg:gap-10 lg:py-10">
+				<div className="lg:flexCol flex animate-translateX gap-2 py-3 sm:gap-8 lg:animate-translateY lg:gap-16 lg:py-10">
 					{categories.map((el, index) => (
 						<button
 							key={index}
