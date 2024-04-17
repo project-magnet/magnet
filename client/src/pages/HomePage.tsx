dad8cd4af375d64af87726e788cf920310a71076
qpwoei0123 <qpwoei0123@naver.com>
2024-04-17 14:21:57 +0900

    refactor: 카테고리 데이터 호출방식 변경

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,44 +1,15 @@
 import {useNavigate} from 'react-router-dom';
+import {categories} from '../asset/categories';
 
 const HomePage = () => {
 	const navigate = useNavigate();
-
-	const categories = [
-		{title: '웹 디자인', id: 'WEB_DESIGN', icon: 'brush-2-line'},
-		{title: 'UI/UX', id: 'UI_UX', icon: 'layout-2-line'},
-		{
-			title: '프로덕트 매니저',
-			id: 'PRODUCT_MANAGER',
-			icon: 'file-list-2-line',
-		},
-		{title: '백엔드', id: 'BACKEND', icon: 'send-to-back'},
-		{title: '프론트엔드', id: 'FRONTEND', icon: 'bring-to-front'},
-		{title: '데브옵스', id: 'DEVOPS', icon: 'cloud-line'},
-		{
-			title: '데이터 엔지니어',
-			id: 'DATA_ENGINEER',
-			icon: 'database-2-line',
-		},
-		{
-			title: '서버 엔자니어',
-			id: 'SERVER_ENGINEER',
-			icon: 'server-line',
-		},
-		{title: 'AI', id: 'AI', icon: 'robot-3-line'},
-	];
-	const onclick = () => {
-		console.log(process.env.REACT_APP_BASE_URL, 'process.env.REACT_APP_BASE_URL');
-		console.log(process.env.CLIENT_ID, 'process.env.CLIENT_ID');
-	};
 
 	return (
 		<div className="flexCol rootPageSection justify-center gap-10 overflow-hidden lg:flex-row">
 			<section className="flexCenter animate-showSideRight flex-col  font-PartialSansKR_Regular">
 				<div className="text-[3rem] sm:text-[4rem] lg:text-[6rem] 2xl:text-[10rem]">
-					<p className="" onClick={onclick}>
-						모두를 위한
-					</p>
-					<p className="">멘토링 서비스</p>
+					<p>모두를 위한</p>
+					<p>멘토링 서비스</p>
 					<div className="flex text-additional2">
 						{['마', '그', '넷', '!'].map((el, index) => (
 							<span className="hover:animate-tickle" key={index}>
