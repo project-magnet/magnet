42dc567eb4b2db0822b2b10fe06b960336848472
qpwoei0123 <qpwoei0123@naver.com>
2024-04-22 19:53:47 +0900

    feat: placeholder 추가

--- client/src/component/input/MyQuillComponent.tsx
+++ client/src/component/input/MyQuillComponent.tsx
@@ -2,17 +2,20 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css'; // Quill의 스노우 테마를 import합니다.
 
 interface MyQuillComponentProps {
-  value: string;
-  setValue: (value: string) => void;
+	value: string;
+	setValue: (value: string) => void;
 }
 
-export const MyQuillComponent = ({ value, setValue }: MyQuillComponentProps) => {
-  return (
-    <ReactQuill
-      theme="snow"
-      value={value}
-      onChange={setValue}
-      style={{ height: '200px', margin: '0 0 30px 0' }}
-    />
-  );
+export const MyQuillComponent = ({value, setValue}: MyQuillComponentProps) => {
+	return (
+		<ReactQuill
+			theme="snow"
+			value={value}
+			onChange={setValue}
+			style={{height: '200px', margin: '0 0 30px 0'}}
+			placeholder="멘토링 내용을 입력해주세요."
+		/>
+	);
 };
+
+export default MyQuillComponent;
