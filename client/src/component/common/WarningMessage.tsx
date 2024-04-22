263f1f57f27efb69e9b363eb263ab30ed92d2e2b
qpwoei0123 <qpwoei0123@naver.com>
2024-04-22 19:53:09 +0900

    refactor: 기능 간소화

--- client/src/component/common/WarningMessage.tsx
+++ client/src/component/common/WarningMessage.tsx
@@ -1,18 +1,8 @@
 type WarningMessageProps = {
 	message: string;
 	isSuccess?: boolean;
-	visible?: boolean;
 };
 
-export const WarningMessage = ({
-	message,
-	isSuccess = false,
-	visible = true,
-}: WarningMessageProps) => {
-	if (!visible) return null;
-
-	const className = isSuccess ? 'success animate-disappear' : 'warning animate-shake';
-	const content = isSuccess ? '올바른 값입니다.' : message;
-
-	return <p className={className}>{content}</p>;
+export const WarningMessage = ({message, isSuccess = false}: WarningMessageProps) => {
+	return isSuccess ? null : <p className={`${'warning animate-shake'} my-1`}>{message}</p>;
 };
