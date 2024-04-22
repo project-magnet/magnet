16c0b7474e9e263e35ddd952d632a3d3c4f01270
qpwoei0123 <qpwoei0123@naver.com>
2024-04-22 19:55:39 +0900

    design: 토스트팝업 그림자 적용

--- client/src/component/common/ToastPopup.tsx
+++ client/src/component/common/ToastPopup.tsx
@@ -22,7 +22,7 @@ export const ToastPopup = () => {
 			{isOpen && (
 				<aside className="fixed left-1/2 top-3 z-50 w-10/12 -translate-x-1/2 sm:w-1/2">
 					<div
-						className={`flexCenter h-12 animate-toastPopupMove gap-2 rounded-md bg-slate-100 ${color}`}
+						className={`flexCenter h-12 animate-toastPopupMove gap-2 rounded-md bg-slate-50 drop-shadow-xl ${color}`}
 					>
 						<i className={`ri-${icon} ri-2x`} />
 						<p>{message}</p>
