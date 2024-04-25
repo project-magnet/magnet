8a095be61176e810cd22539aea5e46794d9da001
qpwoei0123 <qpwoei0123@naver.com>
2024-04-25 20:04:12 +0900

    chore: remixicon 선언 위치 변경

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'; // * BrowserRouter 불러오기
+import {BrowserRouter} from 'react-router-dom'; // * BrowserRouter 불러오기
 import App from './App';
 import './styles/tailwind.css';
 import ScrollToTop from './utils/common/scrollToTop';
+import 'remixicon/fonts/remixicon.css';
 
 // * App 을 BrowserRouter 로 감싸기
 ReactDOM.render(
-  <BrowserRouter>
-    <ScrollToTop />
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root'),
+	<BrowserRouter>
+		<ScrollToTop />
+		<App />
+	</BrowserRouter>,
+	document.getElementById('root'),
 );
