bf85b5cc605c20f1685ccf2b01c2031a125577a2
qpwoei0123 <qpwoei0123@naver.com>
2024-05-01 19:44:16 +0900

    refactor: 리액트 포탈 적용

--- client/src/component/common/Modal.tsx
+++ client/src/component/common/Modal.tsx
@@ -1,25 +1,25 @@
+import ReactDOM from 'react-dom';
 import ModalStore from '../../store/ModalStore';
 import {LogoTickle} from './LogoTickle';
 
 export const Modal = () => {
-	const {isOpen, setIsOpenFalse, children} = ModalStore();
+	const {modalChildren, closeModal} = ModalStore();
 
-	const closeModal = () => {
-		setIsOpenFalse();
-	};
-
-	return isOpen ? (
-		<div
-			onClick={closeModal}
-			className="flexCenter fixed top-0 z-50 size-full animate-fadeIn bg-black bg-opacity-50 backdrop-blur-sm "
-		>
-			<section onClick={e => e.stopPropagation()} className="modalStyle relative">
-				<PopupCloseButton handleClick={closeModal} />
-				<LogoTickle word="MAGNET" />
-				{children}
-			</section>
-		</div>
-	) : null;
+	return modalChildren
+		? ReactDOM.createPortal(
+				<div
+					onClick={closeModal}
+					className="flexCenter fixed top-0 z-50 size-full animate-fadeIn bg-black bg-opacity-50 backdrop-blur-sm"
+				>
+					<section onClick={e => e.stopPropagation()} className="modalStyle relative">
+						<PopupCloseButton handleClick={closeModal} />
+						<LogoTickle word="MAGNET" />
+						{modalChildren}
+					</section>
+				</div>,
+				document.body, // 여기가 포탈의 타겟 위치입니다.
+			)
+		: null;
 };
 
 const PopupCloseButton = ({handleClick}: {handleClick: React.MouseEventHandler<HTMLElement>}) => {
@@ -27,7 +27,7 @@ const PopupCloseButton = ({handleClick}: {handleClick: React.MouseEventHandler<H
 		<div className="absolute right-1 top-1">
 			<i
 				onClick={handleClick}
-				className="ri-close-line cursor-pointer text-2xl  text-slate-400 transition-colors duration-300 hover:text-black"
+				className="ri-close-line cursor-pointer text-3xl text-slate-400 transition-colors duration-300 hover:text-black"
 			></i>
 		</div>
 	);
