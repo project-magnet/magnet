1b29fd38588313e870f49c70bb70c0ee409841b2
qpwoei0123 <qpwoei0123@naver.com>
2024-05-07 00:01:53 +0900

    feat: 멘티 리스트 모달 열기 기능 추가

--- client/src/component/user/CreatedMentoringBox.tsx
+++ client/src/component/user/CreatedMentoringBox.tsx
@@ -1,5 +1,6 @@
-import {useState} from 'react';
 import {getMenteeList} from '../../api/mentee';
+import {useOpenMenteeListModal} from '../../hooks/useOpenModals';
+import {useState} from 'react';
 
 type MentoringDto = {
 	id: number;
@@ -11,60 +12,77 @@ type MentoringDto = {
 	category: string;
 };
 
+type MenteeData = {
+	menteeId: number;
+	menteeNickName: string;
+	schedule: string;
+	phone: any;
+	email: string;
+};
+
 export const CreatedMentoringBox = ({mentoringDto}: {mentoringDto: MentoringDto}) => {
-	const [isClicked, setIsClicked] = useState(false);
-	const [mentoringData, setMentoringData] = useState<any>(null); // mentoring 데이터 상태
+	const openMenteeListModal = useOpenMenteeListModal(); // 멘티 리스트 모달 열기
 
 	// 버튼 클릭 시 mentoring 데이터 가져오기
 	const handleClick = async () => {
-		if (mentoringData === null) {
-			try {
-				const data = await getMenteeList(mentoringDto.id); // mentoring 데이터 가져오기
-				setMentoringData(data); // mentoring 데이터 설정
-			} catch (error) {
-				console.error('getMentoring 호출 중 오류 발생:', error);
-			}
+		try {
+			const data = await getMenteeList(mentoringDto.id); // mentoring 데이터 가져오기
+			openMenteeListModal(data); // 멘티 리스트 모달 열기
+		} catch (error) {
+			console.error('getMentoring 호출 중 오류 발생:', error);
 		}
-		setIsClicked(!isClicked); // isClicked 상태 토글
 	};
 
 	return (
-		<div className="flexCol w-full rounded-lg border">
-			<button
-				className={`interactionPushDown textSmall flexCenter w-full justify-between p-5`}
-				onClick={handleClick}
-			>
+		<div className="interactionPushDown flexCol w-full rounded-lg border">
+			<button className={` textSmall flexCenter w-full justify-between p-5`} onClick={handleClick}>
 				<div className="flexCol items-start gap-3">
 					<span className="font-PartialSansKR_Regular">{mentoringDto.category}</span>
 					<span>{mentoringDto.title}</span>
 				</div>
-				<i className={`ri-arrow-drop-${isClicked ? 'down' : 'right'}-line ri-3x`} />
+				<i className={`ri-list-check ri-2x animate-pulse`} />
 			</button>
-			{isClicked &&
-				mentoringData && ( // 버튼이 클릭되었고 mentoring 데이터가 있을 때만 표시
-					<div className={`textSmall flexCol gap-3 p-5 pt-0`}>
-						{/* mentoring 데이터 표시 */}
-						<div>
-							<p className="textBase font-black">멘토링 내용</p>
-							<div
-								className="text-pretty text-secondary "
-								dangerouslySetInnerHTML={{__html: mentoringData.content}}
-							/>
-						</div>
-						<div>
-							<p className="textBase">가격</p>
-							<span className="text-secondary">{mentoringData.pay}</span>
-						</div>
-						<div>
-							<p className="textBase">참여인원</p>
-							<span className="text-secondary">{mentoringData.participants}</span>
-						</div>
-						<div>
-							<p className="textBase">기간</p>
-							<span className="text-secondary">{mentoringData.period}</span>
-						</div>
+		</div>
+	);
+};
+
+export const MenteeListModal = ({menteeList}: {menteeList: MenteeData[]}) => {
+	return (
+		<div className="flexCol h-96 w-full gap-3 overflow-scroll">
+			<span className="font-PartialSansKR_Regular"> 멘티 리스트 </span>
+			{menteeList.map((mentee, index) => (
+				<MennteeBox mentee={mentee} key={index} />
+			))}
+		</div>
+	);
+};
+
+const MennteeBox = ({mentee, key}: {mentee: MenteeData; key: number}) => {
+	const [isShow, setIsShow] = useState(false);
+
+	return (
+		<div
+			key={key}
+			className={`interactionPushDown flexCol w-full gap-5 rounded-lg border p-3`}
+			onClick={() => setIsShow(!isShow)}
+		>
+			<div className="flexCenter w-full justify-between">
+				<span className="textLarge">{mentee.menteeNickName}</span>
+				<i className={`ri-arrow-drop-${isShow ? 'down' : 'right'}-line ri-2x`} />
+			</div>
+
+			{isShow && (
+				<div className="flexCol textSmall animate-fadeIn gap-3">
+					<div>
+						<p className="textBase font-black">Phone</p>
+						<span className="text-secondary">{mentee.phone || '관리자에게 문의해주세요!'}</span>
 					</div>
-				)}
+					<div>
+						<p className="textBase font-black">Email</p>
+						<span className="text-secondary">{mentee.email || '관리자에게 문의해주세요!'}</span>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 };
