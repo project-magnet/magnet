4eacbd4ee6f163e7f84667aaaf9c38f03b464547
Lee dong yeol <dlehdduf0902@naver.com>
2024-05-11 03:00:29 +0900

    [refactor]Mentoring list fetchjoin 적용

--- src/main/java/com/example/magnet/mentoring/repository/GetMentoringRepositoryImpl.java
+++ src/main/java/com/example/magnet/mentoring/repository/GetMentoringRepositoryImpl.java
@@ -52,7 +52,7 @@ public class GetMentoringRepositoryImpl implements GetMentoringRepository{
                         mentor.mentorName,
                         mentor.career))
                 .from(mentoring)
-                .leftJoin(mentoring.mentor, mentor)
+                .leftJoin(mentoring.mentor, mentor).fetchJoin()
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
