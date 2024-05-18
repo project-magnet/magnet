a3c310029b51bef29aeff7467326e7dfb6f44ac2
qpwoei0123 <qpwoei0123@naver.com>
2024-05-18 17:24:08 +0900

    feat: 멘토리스트 size 셀렉터 추가

--- client/src/pages/MentorListPage.tsx
+++ client/src/pages/MentorListPage.tsx
@@ -5,25 +5,32 @@ import {getMentoringList, Content} from '../api/mentoring';
 import {categories} from '../asset/categories';
 
 const MentoringListPage = () => {
+	const [loading, setLoading] = useState(true);
 	const [category, setCategory] = useState('ALL');
-	const [mentoringList, setMentoringList] = useState<Content[] | null>(null);
+	const [mentoringList, setMentoringList] = useState<Content[]>([]);
 	const [currentPage, setCurrentPage] = useState(1);
 	const [totalPages, setTotalPages] = useState(0);
+	const [size, setSize] = useState(3);
 	const newCategories = [{id: 'ALL', title: '전체', icon: 'menu-line'}].concat(categories);
+	const filteredMentoringList = mentoringList.filter(
+		el => el.category === category || category === 'ALL',
+	);
 
 	useEffect(() => {
 		const fetchMentoringList = async () => {
 			try {
-				const data = await getMentoringList(currentPage, 3);
+				const data = await getMentoringList(currentPage, size);
 				setMentoringList(data.content);
 				setTotalPages(data.totalPages);
 				console.log(data);
 			} catch (error) {
 				console.error('멘토 리스트를 불러오는 동안 오류가 발생했습니다:', error);
+			} finally {
+				setLoading(false);
 			}
 		};
 		fetchMentoringList();
-	}, [currentPage]);
+	}, [currentPage, size]);
 
 	const handlePreviousPage = () => {
 		setCurrentPage(prevPage => prevPage - 1);
@@ -49,34 +56,59 @@ const MentoringListPage = () => {
 					</button>
 				))}
 			</div>
-			<div className="flexCenter w-full flex-wrap gap-10">
-				{mentoringList ? (
-					mentoringList
-						.filter(el => el.category === category || category === 'ALL')
-						.map((el, index) => <MentorCard mentoring={el} key={index} />)
-				) : (
-					<LodingContainer />
-				)}
-			</div>
-			<div className="flexCenter mt-4">
-				<button
-					onClick={handlePreviousPage}
-					disabled={currentPage === 1}
-					className="buttonStyleTertiary font-semibold"
-				>
-					이전
-				</button>
-				<p className="px-4 py-2 font-semibold text-gray-700">
-					{currentPage} / {totalPages}
-				</p>
-				<button
-					onClick={handleNextPage}
-					disabled={currentPage === totalPages}
-					className={`buttonStyleTertiary font-semibold`}
-				>
-					다음
-				</button>
-			</div>
+			{loading ? (
+				<LodingContainer />
+			) : filteredMentoringList.length === 0 ? (
+				<div className="flexCol mt-20 items-center gap-2">
+					<img
+						src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Face%20with%20Monocle.png"
+						alt="Crying Face"
+						width="100"
+						className="animate-downALittle"
+					/>
+					<p className="animate-fadeIn font-bold transition-opacity delay-300">
+						아무리 찾아봐도 없네요...
+					</p>
+				</div>
+			) : (
+				<>
+					<div className="mt-4 flex w-full justify-end px-10">
+						<select
+							value={size}
+							onChange={e => setSize(parseInt(e.target.value))}
+							className="buttonStyleTertiary outline-none"
+						>
+							<option value="3">3개씩 보기</option>
+							<option value="6">6개씩 보기</option>
+							<option value="9">9개씩 보기</option>
+						</select>
+					</div>
+					<div className="flexCenter w-full flex-wrap gap-10">
+						{filteredMentoringList.map((el, index) => (
+							<MentorCard key={index} mentoring={el} />
+						))}
+					</div>
+					<div className="flexCenter mt-4">
+						<button
+							onClick={handlePreviousPage}
+							disabled={currentPage === 1}
+							className="buttonStyleTertiary font-semibold"
+						>
+							이전
+						</button>
+						<p className="px-4 py-2 font-semibold text-gray-700">
+							{currentPage} / {totalPages}
+						</p>
+						<button
+							onClick={handleNextPage}
+							disabled={currentPage === totalPages}
+							className={`buttonStyleTertiary font-semibold`}
+						>
+							다음
+						</button>
+					</div>
+				</>
+			)}
 		</section>
 	);
 };
